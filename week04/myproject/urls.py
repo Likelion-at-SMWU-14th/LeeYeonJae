4eea54e59406f8db8dcaf92930842555f995a063
lyeonj <lyeonj@sookmyung.ac.kr>
2026-03-31 21:12:26 +0900

    feat: 회원가입, 로그인 추가

--- week04/myproject/urls.py
+++ week04/myproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('', views.login, name='login'),
 ]
