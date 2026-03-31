89b14f6451daafae4a7a7788d7d6da864a6c70bd
lyeonj <lyeonj@sookmyung.ac.kr>
2026-03-31 21:22:44 +0900

    feat: 로그아웃 기능 추가

--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 def signup(request):
     if request.method == "POST":
@@ -29,3 +29,6 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
         return redirect('login')
+
+def main(request):
+    return render(request, "main.html")
