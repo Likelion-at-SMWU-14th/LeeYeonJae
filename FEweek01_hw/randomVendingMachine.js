6c024d65c0f772e0ae8bb87c30f6983d05f6f942
lyeonj <lyeonj@sookmyung.ac.kr>
2026-04-12 23:26:05 +0900

    feat: 데이터 설정

---
+++ FEweek01_hw/randomVendingMachine.js
@@ -0,0 +1,15 @@
+console.log("JS로 랜덤 자판기 만들기");
+
+// 음료 리스트
+const drinks = [
+    { name: "🍎apple", price: 5000 },
+    { name: "🍊orange", price: 5500 },
+    { name: "🍇grape", price: 6000 },
+    { name: "🥝kiwi", price: 6500 },
+    { name: "🍓strawberry", price: 7000 },
+    { name: "🍋lemon", price: 7500 },
+    { name: "🫐blueberry", price: 8000 },
+];
+
+// 지갑 금액
+let money = 7000;
