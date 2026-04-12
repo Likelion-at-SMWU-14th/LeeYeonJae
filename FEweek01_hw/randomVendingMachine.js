b2a1155c345342e7b61b9639a07ca374d904974c
lyeonj <lyeonj@sookmyung.ac.kr>
2026-04-12 23:50:43 +0900

    feat: 함수 작성
    - 출력 문자열 반환 함수 구현
    - 음료 출력 함수 구현

--- FEweek01_hw/randomVendingMachine.js
+++ FEweek01_hw/randomVendingMachine.js
@@ -14,27 +14,36 @@ const drinks = [
 // 지갑 금액
 let money = 7000;
 
-// 음료 1개 랜덤 선택
+// 음료 랜덤 선택
 function getRandomDrink() {
     return drinks[Math.floor(Math.random() * drinks.length)];
 }
 
-// 음료 출력 함수
-function buyRandomDrink() {
-    const randomDrink = getRandomDrink();
-
-    if (randomDrink.price <= money) {
-        money -= randomDrink.price;
-
-        console.log(`${randomDrink.name} 음료가 나왔어요! (가격: ${randomDrink.price}원)`);
-        console.log(`지갑에 남은 돈: ${money}원`);
+// 출력 문자열 반환
+function getResult(drink, canBuy) {
+    if (canBuy) {
+        return `${drink.name} 음료가 나왔어요! (가격: ${drink.price}원)\n지갑에 남은 돈: ${money}원`;
     } else {
-        console.log("돈이 부족해요! 음료를 살 수 없어요!");
+        return `돈이 부족해요! 음료를 살 수 없어요!`;
+    }
+
+}
+
+// 음료 출력 함수
+function buyDrink() {
+    for (let i = 0; i < 3; i++) {
+        console.log(`\n[${i + 1}번째 시도]`);
+
+        const drink = getRandomDrink();
+
+        if (drink.price <= money) {
+            money -= drink.price;
+            console.log(getResult(drink, true));
+        } else {
+            console.log(getResult(drink, false));
+        }
     }
 }
 
-// 3번 반복
-for (let i = 0; i < 3; i++) {
-    console.log(`[${i + 1}번째 시도]`);
-    buyRandomDrink();
-}
+// 호출
+buyDrink();
