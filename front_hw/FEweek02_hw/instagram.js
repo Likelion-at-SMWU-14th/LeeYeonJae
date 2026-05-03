984f1d2540d29a2d0797a9486971b743ec29f8c7
lyeonj <lyeonj@sookmyung.ac.kr>
2026-05-03 22:26:09 +0900

    feat: DOM 요소 접근을 위한 변수 선언

---
+++ front_hw/FEweek02_hw/instagram.js
@@ -0,0 +1,8 @@
+// DOM 요소 접근
+const likeBtn = document.getElementById("like-btn");
+const commentBtn = document.getElementById("comment-btn");
+const shareBtn = document.getElementById("share-btn");
+const saveBtn = document.getElementById("save-btn");
+
+const menuBtn = document.querySelector("#menu-btn");
+const moreBtn = document.querySelector("#more");
