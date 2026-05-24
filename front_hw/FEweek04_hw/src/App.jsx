c7143b66052eb37c27a87c87c87ad5eb8df699e2
lyeonj <lyeonj@sookmyung.ac.kr>
2026-05-24 23:21:01 +0900

    feat: Frame 컴포넌트 구현

--- front_hw/FEweek04_hw/src/App.jsx
+++ front_hw/FEweek04_hw/src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import styled from "styled-components";
+import Frame from "./components/Frame";
 
 function App() {
-  return <></>;
+  return (
+    <Wrapper>
+      <Frame></Frame>
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
