7f93778a56a8d536e4ffb43395371a82fe0a490e
lyeonj <lyeonj@sookmyung.ac.kr>
2026-06-30 16:29:29 +0900

    feat: useEffect 의존성 배열 실습

--- front/FEweek06/src/App.jsx
+++ front/FEweek06/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import UserInfo from "./components/Info";
+import DebouncedSearch from "./components/DebouncedSearch";
 
 function App() {
   return (
     <div>
       <UserInfo />
+      <DebouncedSearch />
     </div>
   );
 }
--- front/FEweek06/src/components/Info.jsx
+++ front/FEweek06/src/components/Info.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function UserInfo() {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  // 모든 렌더링마다 출력
+  useEffect(() => {
+    console.log("렌더링이 되었어요!");
+    console.log({ name, nickname });
+  });
+
+  // 최초 1번만 출력
+  useEffect(() => {
+    console.log("최초 렌더링시 출력되어요!");
+  }, []);
+
+  // nickname의 state가 바뀔 때마다 출력
+  useEffect(() => {
+    console.log("nickname이 변경될 때마다 렌더링!");
+    console.log({ nickname });
+  }, [nickname]);
 
   return (
     <div>
