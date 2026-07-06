5826f3e6207244b594c92aacfff40e41f0172d5e
lyeonj <lyeonj@sookmyung.ac.kr>
2026-07-07 02:16:29 +0900

    chore: 멜론 데이터 및 이미지 추가

---
+++ front_hw/FEweek05_hw/src/data/musicData.js
@@ -0,0 +1,93 @@
+import cover1 from "../assets/1.jpg";
+import cover2 from "../assets/2.jpg";
+import cover3 from "../assets/3.jpg";
+import cover4 from "../assets/4.jpg";
+import cover5 from "../assets/5.jpg";
+import cover6 from "../assets/6.jpg";
+import cover7 from "../assets/7.jpg";
+import cover8 from "../assets/8.jpg";
+import cover9 from "../assets/9.jpg";
+import cover10 from "../assets/10.jpg";
+
+export const music = [
+  {
+    rank: 1,
+    image: cover1,
+    title: "잠자기",
+    artist: "아이오아이 (I.O.I)",
+    album: "I.O.I 3rd MINI ALBUM",
+    likes: "62,661",
+  },
+  {
+    rank: 2,
+    image: cover2,
+    title: "REDRED",
+    artist: "CORTIS (코르티스)",
+    album: "GREENGREEN",
+    likes: "71,414",
+  },
+  {
+    rank: 3,
+    image: cover3,
+    title: "LOVE ATTACK",
+    artist: "RESCENE (리센느)",
+    album: "SCENEDROME",
+    likes: "94,595",
+  },
+  {
+    rank: 4,
+    image: cover4,
+    title: "LEMONADE",
+    artist: "aespa",
+    album: "LEMONADE - The 2nd Album",
+    likes: "44,831",
+  },
+  {
+    rank: 5,
+    image: cover5,
+    title: "It's Me",
+    artist: "아일릿 (ILLIT)",
+    album: "MAMIHILAPINATAPAI",
+    likes: "47,360",
+  },
+  {
+    rank: 6,
+    image: cover6,
+    title: "소문의 낙원",
+    artist: "AKMU (악뮤)",
+    album: "개화",
+    likes: "84,848",
+  },
+  {
+    rank: 7,
+    image: cover7,
+    title: "RUDE!",
+    artist: "Hearts2Hearts (하츠투하츠)",
+    album: "RUDE!",
+    likes: "69,176",
+  },
+  {
+    rank: 8,
+    image: cover8,
+    title: "캐치 캐처",
+    artist: "YENA (최예나)",
+    album: "LOVE CATCHER",
+    likes: "43,440",
+  },
+  {
+    rank: 9,
+    image: cover9,
+    title: "사랑하게 될 거야",
+    artist: "한로로",
+    album: "이상비행",
+    likes: "141,639",
+  },
+  {
+    rank: 10,
+    image: cover10,
+    title: "Drowning",
+    artist: "WOODZ",
+    album: "OO-LI",
+    likes: "303,797",
+  },
+];
