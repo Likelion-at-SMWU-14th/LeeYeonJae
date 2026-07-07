f2802dd736ecf833e3e37ac55d138a41e17c46ca
lyeonj <lyeonj@sookmyung.ac.kr>
2026-07-07 16:57:10 +0900

    feat: 중첩라우팅, useNavigate 실습

--- front/FEweek07/src/App.jsx
+++ front/FEweek07/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import QuizPage from "./pages/QuizPage";
 import ResultPage from "./pages/ResultPage";
 
@@ -8,8 +9,11 @@ function App() {
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route path="/" element={<QuizPage />} />
-          <Route path="/result" element={<ResultPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<QuizPage />} />
+            <Route path="quiz/:quizId" element={<QuizPage />} />
+            <Route path="/result" element={<ResultPage />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
