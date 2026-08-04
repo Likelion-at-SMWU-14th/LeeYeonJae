c888a008333286abf6e7d40f929e6550b230b3a5
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-04 15:50:19 +0900

    feat: TitleContext 적용

--- front/FEweek11/src/App.jsx
+++ front/FEweek11/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import TitleContext from "./contexts/TitleContext";
 
 function App() {
   return (
-    <>
+    <TitleContext.Provider value={"yeonjae의 영화관"}>
       <Header />
       <Outlet />
-    </>
+    </TitleContext.Provider>
   );
 }
 
