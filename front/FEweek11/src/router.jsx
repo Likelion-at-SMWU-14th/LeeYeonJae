316a9a8fcf7950facc948ca9773419571fa6cca8
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-04 15:37:57 +0900

    setting: 초기 코드 작성

--- front/FEweek11/src/router.jsx
+++ front/FEweek11/src/router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import ReviewPage from "./pages/ReviewPage";
+import CreatePage from "./pages/CreatePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true, // path: "/" 와 동일
+        element: <ReviewPage />,
+      },
+      {
+        path: "create",
+        element: <CreatePage />,
+      },
+    ],
   },
 ]);
 
