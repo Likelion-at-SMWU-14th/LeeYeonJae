5ff80e0f5ac1b992d222d9ef7d750251b0d21b5e
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-09 21:10:10 +0900

    feat: PartProvider 적용

--- front_hw/FEweek11_hw/src/main.jsx
+++ front_hw/FEweek11_hw/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { PartProvider } from "./contexts/PartContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <PartProvider>
+      <App />
+    </PartProvider>
   </StrictMode>,
-)
+);
