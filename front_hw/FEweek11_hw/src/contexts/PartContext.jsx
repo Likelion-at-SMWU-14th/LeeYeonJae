3014eeb671800e95fefda9ff7e925dad6903500b
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-09 18:41:10 +0900

    feat: PartProvider 컴포넌트 생성

--- front_hw/FEweek11_hw/src/contexts/PartContext.jsx
+++ front_hw/FEweek11_hw/src/contexts/PartContext.jsx
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const PartContext = createContext();
+
+export const PartProvider = ({ children }) => {
+  const [part, setPart] = useState("");
+
+  return (
+    <PartContext.Provider value={{ part, setPart }}>
+      {children}
+    </PartContext.Provider>
+  );
+};
 
 export default PartContext;
