5fb480f9752646f593f2b7aaa31c4885bb2efa12
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-09 21:05:40 +0900

    feat: PartContext 적용

--- front_hw/FEweek11_hw/src/App.jsx
+++ front_hw/FEweek11_hw/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useContext } from "react";
 import Content from "./components/Content";
 import { styled } from "styled-components";
+import PartContext from "./contexts/PartContext";
 
 function App() {
-  const [part, setPart] = useState("");
+  const { part } = useContext(PartContext);
+
   return (
     <Wrapper>
       <div className="title">
         숙명여대 멋쟁이사자처럼 <span>{part}</span> 파트 구성원
       </div>
-      <Content part={part} setPart={setPart} />
+      <Content />
     </Wrapper>
   );
 }
