ebc8373535550e3c2bb3a20c3b070e591488e59c
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-15 04:12:21 +0900

    feat: persist middleware 적용

--- front_hw/FEweek12_hw/src/stores/cartStore.js
+++ front_hw/FEweek12_hw/src/stores/cartStore.js
@@ -1,91 +1,93 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 const useCartStore = create(
-  devtools((set) => ({
-    items: [],
+  devtools(
+    persist((set) => ({
+      items: [],
 
-    // 상품 추가
-    addItem: (product) =>
-      set(
-        (state) => {
-          const existingItem = state.items.find(
-            (item) => item.id === product.id,
-          );
+      // 상품 추가
+      addItem: (product) =>
+        set(
+          (state) => {
+            const existingItem = state.items.find(
+              (item) => item.id === product.id,
+            );
 
-          if (existingItem) {
+            if (existingItem) {
+              return {
+                items: state.items.map((item) =>
+                  item.id === product.id
+                    ? {
+                        ...item,
+                        quantity: item.quantity + 1,
+                      }
+                    : item,
+                ),
+              };
+            }
+
             return {
-              items: state.items.map((item) =>
-                item.id === product.id
-                  ? {
-                      ...item,
-                      quantity: item.quantity + 1,
-                    }
-                  : item,
-              ),
+              items: [
+                ...state.items,
+                {
+                  ...product,
+                  quantity: 1,
+                },
+              ],
             };
-          }
+          },
+          undefined,
+          "cart/addItem",
+        ),
 
-          return {
-            items: [
-              ...state.items,
-              {
-                ...product,
-                quantity: 1,
-              },
-            ],
-          };
-        },
-        undefined,
-        "cart/addItem",
-      ),
+      // 상품 삭제
+      removeItem: (productId) =>
+        set(
+          (state) => ({
+            items: state.items.filter((item) => item.id !== productId),
+          }),
+          undefined,
+          "cart/removeItem",
+        ),
 
-    // 상품 삭제
-    removeItem: (productId) =>
-      set(
-        (state) => ({
-          items: state.items.filter((item) => item.id !== productId),
-        }),
-        undefined,
-        "cart/removeItem",
-      ),
-
-    // 수량 변경(+)
-    increaseQuantity: (productId) =>
-      set(
-        (state) => ({
-          items: state.items.map((item) =>
-            item.id === productId
-              ? {
-                  ...item,
-                  quantity: item.quantity + 1,
-                }
-              : item,
-          ),
-        }),
-        undefined,
-        "cart/increaseQuantity",
-      ),
-
-    // 수량 변경(-)
-    decreaseQuantity: (productId) =>
-      set(
-        (state) => ({
-          items: state.items
-            .map((item) =>
+      // 수량 변경(+)
+      increaseQuantity: (productId) =>
+        set(
+          (state) => ({
+            items: state.items.map((item) =>
               item.id === productId
                 ? {
                     ...item,
-                    quantity: item.quantity - 1,
+                    quantity: item.quantity + 1,
                   }
                 : item,
-            )
-            .filter((item) => item.quantity > 0),
-        }),
-        undefined,
-        "cart/decreaseQuantity",
-      ),
-  })),
+            ),
+          }),
+          undefined,
+          "cart/increaseQuantity",
+        ),
+
+      // 수량 변경(-)
+      decreaseQuantity: (productId) =>
+        set(
+          (state) => ({
+            items: state.items
+              .map((item) =>
+                item.id === productId
+                  ? {
+                      ...item,
+                      quantity: item.quantity - 1,
+                    }
+                  : item,
+              )
+              .filter((item) => item.quantity > 0),
+          }),
+          undefined,
+          "cart/decreaseQuantity",
+        ),
+    })),
+  ),
 );
 
 export default useCartStore;
