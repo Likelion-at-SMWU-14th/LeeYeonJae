1e0727b0b0a5470dfdd5ae381848cc4f316f7cb0
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-15 03:25:08 +0900

    feat: ShopPage 구현

--- front_hw/FEweek12_hw/src/App.jsx
+++ front_hw/FEweek12_hw/src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import GlobalStyle from "./styles/GlobalStyle";
+import ShopPage from "./pages/ShopPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <ShopPage />
+    </>
+  );
 }
 
 export default App;
