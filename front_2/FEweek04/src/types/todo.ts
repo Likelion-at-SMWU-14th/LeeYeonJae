29cf9d1a9cc990b14b86d3765b25bd3950d23e58
lyeonj <lyeonj@sookmyung.ac.kr>
2026-09-22 19:37:06 +0900

    feat: Todo 타입에 선택적 속성과 readonly 적용

--- front_2/FEweek04/src/types/todo.ts
+++ front_2/FEweek04/src/types/todo.ts
@@ -3,8 +3,8 @@ export type TodoPriority = "low" | "medium" | "high";
 export type TodoFilter = "all" | "active" | "completed";
 
 export interface Todo {
-  id: number;
+  readonly id: number;
   title: string;
   completed: boolean;
-  priority: TodoPriority;
+  priority?: TodoPriority;
 }
