e85308de1b374bbd85424c5c1289d5df8dcd4ef3
lyeonj <lyeonj@sookmyung.ac.kr>
2026-09-22 19:44:52 +0900

    feat: filterTodos와 TodoStore 타입 정의

--- front_2/FEweek04/src/types/todo.ts
+++ front_2/FEweek04/src/types/todo.ts
@@ -8,3 +8,14 @@ export interface Todo {
   completed: boolean;
   priority?: TodoPriority;
 }
+
+export interface TodoStore {
+  todos: Todo[];
+  filter: TodoFilter;
+
+  addTodo: (title: string, priority: TodoPriority) => void;
+
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+  setFilter: (filter: TodoFilter) => void;
+}
